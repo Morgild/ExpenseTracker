ad7bbd12ddd1f8513ea560187614a36cbf214aec
unknown <morgild@gmail.com>
2025-07-23 13:52:34 +0800

    urban3

--- front/src/app/Dashboard/page.js
+++ front/src/app/Dashboard/page.js
@@ -147,7 +147,24 @@ export default function Dashboard() {
       <HeaderDashboard />
       {addRecord && <AddRecord />}
       {addCat && <AddCategory />}
-      <section
+      <iframe
+        style={{
+          overflow: "hidden",
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          right: "0px",
+          bottom: "0px",
+          width: "100%",
+          height: "100%",
+        }}
+        title="Monartex"
+        // width="100%"
+        // height="100%"
+        src="https://app.powerbi.com/reportEmbed?reportId=5e4bbb3b-8392-46a6-8d22-e8f17b751a92&autoAuth=true&ctid=abde62c4-46fa-4bd7-9585-391b26488815"
+        // allowFullScreen={false}
+      ></iframe>
+      {/* <section
         onClick={() => {
           setProfileLog(false);
         }}
@@ -275,7 +292,7 @@ export default function Dashboard() {
             <DashboardRecords />
           </div>
         </div>
-      </section>
+      </section> */}
     </main>
   );
 }
